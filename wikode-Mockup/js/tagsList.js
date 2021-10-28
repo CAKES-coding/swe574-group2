b4bf83a9ad5c03b9abf8d3bd88f05d5932f2501d
Can <hcdayan@hotmail.com>
2021-10-28 18:13:56 +0300

    Add modal to tag title. Add downvote and downvote cells. Add js for tag list.

---
+++ wikode-Mockup/js/tagsList.js
@@ -0,0 +1,40 @@
+// Tag upvote/downvote Logic
+
+var isUpvoted = false;
+var isDownvoted = false;
+
+// Upvote
+function incrementValue() {
+    var upvoteNumberText = document.getElementById('upvote').innerText;
+    console.log(upvoteNumberText)
+    var upvoteNumber = parseInt(upvoteNumberText);
+    console.log(upvoteNumber)
+
+    if (isUpvoted) {
+        upvoteNumber -= 1;
+        isUpvoted = false
+    } else {
+        upvoteNumber += 1;
+        isUpvoted = true
+    }
+
+    document.getElementById('upvote').innerText = upvoteNumber;
+}
+
+// Downvote
+function decrementValue() {
+    var downvoteNumberText = document.getElementById('downvote').innerText;
+    console.log(downvoteNumberText)
+    var downvoteNumber = parseInt(downvoteNumberText);
+    console.log(downvoteNumber)
+
+    if (isDownvoted) {
+        downvoteNumber += 1;
+        isDownvoted = false
+    } else {
+        downvoteNumber -= 1;
+        isDownvoted = true
+    }
+
+    document.getElementById('downvote').innerText = downvoteNumber;
+}
