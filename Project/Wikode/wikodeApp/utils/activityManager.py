081ff4a2c7ba5412e50c263d6881f36516913628
jajimentol <aniljturan@gmail.com>
2021-12-01 12:00:50 +0300

    init added to activity manager, user_id inserted in init method

--- Project/Wikode/wikodeApp/utils/activityManager.py
+++ Project/Wikode/wikodeApp/utils/activityManager.py
@@ -3,62 +3,65 @@ import datetime
 # the manager that will handle all the activity stream savings to database
 from wikodeApp.models import Activity
 
+
 # Activity Manager will handle all savings to database
 # with Activity Model. Call any method related to the activity
 # to record an activity.
 
 class ActivityManager:
 
+    def __init__(self, user_id):
+        self.user_id = user_id
+
     # This method saves the 'View' activity to the database.
     # user_id: the id of the user who makes the activity
     # target_type: the type of the target, depends on if the target
     #              is a user, article or tag
     # target_id: the id of the target, correlated with target_type
-    def saveViewActivity(user_id, target_type, target_id):
+    def saveViewActivity(self, target_type, target_id):
 
         # TODO: get username from db
-        owner_name='Owner'
+        owner_name = 'Owner'
 
         current_time = datetime.datetime.now()
 
-        if target_type=='1':
-            acitivity_target_type='Person'
-            activity_target_url="http://www.wikode.com/wikode/profile/{}".format(target_id)
-            acitivity_target_name='PersonName'
+        if target_type == '1':
+            activity_target_type = 'Person'
+            activity_target_url = "http://www.wikode.com/wikode/profile/{}".format(target_id)
+            activity_target_name = 'PersonName'
         # elif target_type=='2':
         # TODO: when view tags finished, implement the correct tag url:
-            #  acitivity_target_type = 'Tag'
-            #  activity_target_url = "http://www.wikode.com/wikode/articleDetail/{}".format(target_id)
-            #   acitivity_target_type='TagLabel'
-        elif target_type=='3':
-            acitivity_target_type='Article'
-            activity_target_url="http://www.wikode.com/wikode/articleDetail/{}".format(target_id)
-            acitivity_target_name = 'ArticleTitle'
+        #  activity_target_type = 'Tag'
+        #  activity_target_url = "http://www.wikode.com/wikode/articleDetail/{}".format(target_id)
+        #   activity_target_type='TagLabel'
+        elif target_type == '3':
+            activity_target_type = 'Article'
+            activity_target_url = "http://www.wikode.com/wikode/articleDetail/{}".format(target_id)
+            activity_target_name = 'ArticleTitle'
 
-        json={
-                 "@context": "https://www.w3.org/ns/activitystreams",
-                  "summary": "{} viewed {}".format(owner_name, acitivity_target_name),
-                  "type": "View",
-                  "published": current_time,
-                 "actor": {
-                      "type": "Person",
-                      "id": "http://www.wikode.com/wikode/profile/{}".format(user_id),
-                      "name": owner_name,
-                      "url": "http://www.wikode.com/wikode/profile/{}".format(user_id)
-                 },
-                 "object" : {
-                      "id": activity_target_url,
-                     "type": acitivity_target_type,
-                     "url": activity_target_url,
-                     "name": acitivity_target_name
-                }
+        json = {
+            "@context": "https://www.w3.org/ns/activitystreams",
+            "summary": "{} viewed {}".format(owner_name, activity_target_name),
+            "type": "View",
+            "published": current_time,
+            "actor": {
+                "type": "Person",
+                "id": "http://www.wikode.com/wikode/profile/{}".format(self.user_id),
+                "name": owner_name,
+                "url": "http://www.wikode.com/wikode/profile/{}".format(self.user_id)
+            },
+            "object": {
+                "id": activity_target_url,
+                "type": activity_target_type,
+                "url": activity_target_url,
+                "name": activity_target_name
+            }
         }
 
-        activity=Activity(
-            user_id = user_id,
-            activity_type = 1,
-            target_type = target_type,
-            target_id = target_id,
-            activity_JSON = json
+        activity = Activity(
+            user_id=user_id,
+            activity_type=1,
+            target_type=target_type,
+            target_id=target_id,
+            activity_JSON=json
         )
-
