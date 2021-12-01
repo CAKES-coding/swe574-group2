ee6c1e81c9b9d107f63e57784dd1d5f3fa8b4bf2
jajimentol <aniljturan@gmail.com>
2021-12-01 11:45:08 +0300

    activity Manager started, view saving added

---
+++ Project/Wikode/wikodeApp/utils/activityManager.py
@@ -0,0 +1,64 @@
+import datetime
+
+# the manager that will handle all the activity stream savings to database
+from wikodeApp.models import Activity
+
+# Activity Manager will handle all savings to database
+# with Activity Model. Call any method related to the activity
+# to record an activity.
+
+class ActivityManager:
+
+    # This method saves the 'View' activity to the database.
+    # user_id: the id of the user who makes the activity
+    # target_type: the type of the target, depends on if the target
+    #              is a user, article or tag
+    # target_id: the id of the target, correlated with target_type
+    def saveViewActivity(user_id, target_type, target_id):
+
+        # TODO: get username from db
+        owner_name = 'Owner'
+
+        # TODO: get targetName from db
+        target_name = 'Target'
+        
+        current_time = datetime.datetime.now()
+
+        if target_type == '1':
+            acitivity_target_name = 'Person'
+            activity_target_url = "http://www.wikode.com/wikode/profile/{}".format(target_id)
+        elif target_type == '2':
+          #  TODO: when view tagis finished, implement the correct tag url
+          #  acitivity_target_name = 'Tag'
+          #  activity_target_url = "http://www.wikode.com/wikode/articleDetail/{}".format(target_id)
+        elif target_type == '3':
+            acitivity_target_name = 'Article'
+            activity_target_url = "http://www.wikode.com/wikode/articleDetail/{}".format(target_id)
+
+        json = {
+                 "@context": "https://www.w3.org/ns/activitystreams",
+                  "summary": "{} viewed {}".format(owner_name, target_name),
+                  "type": "View",
+                  "published": current_time,
+                 "actor": {
+                      "type": "Person",
+                      "id": "http://www.wikode.com/wikode/profile/{}".format(user_id),
+                      "name": owner_name,
+                      "url": "http://www.wikode.com/wikode/profile/{}".format(user_id)
+                 },
+                 "object" : {
+                      "id": activity_target_url,
+                     "type": acitivity_target_name,
+                     "url": activity_target_url,
+                     "name": target_name
+                }
+        }
+
+        activity = Activity(
+            user_id = user_id,
+            activity_type = 1,
+            target_type = target_type,
+            target_id = target_id,
+            activity_JSON = json
+        )
+
