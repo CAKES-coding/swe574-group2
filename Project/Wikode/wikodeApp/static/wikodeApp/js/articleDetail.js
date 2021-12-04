c6c03c367fef3a82084360fce934e5457130a24f
Semih Solmaz <semihsolmaz@hotmail.com>
2021-12-05 00:59:43 +0300

    highlight selected text

--- Project/Wikode/wikodeApp/static/wikodeApp/js/articleDetail.js
+++ Project/Wikode/wikodeApp/static/wikodeApp/js/articleDetail.js
@@ -3,17 +3,14 @@ let tagPieceButton = document.getElementById("tag-piece-button")
 
 let startIndex = 0;
 let endIndex = 0;
-
+let selectedText = "";
 
 abstractText.addEventListener("mouseup", () => {
     if (window.getSelection) {
         let selection = window.getSelection()
-        let selectedText = selection.toString();
+        selectedText = selection.toString();
         startIndex = selection.anchorOffset;
         endIndex = selection.focusOffset;
-        let selectionNode = selection.anchorNode;
-        console.log(selectedText);
-        console.log(selectionNode);
     }
 })
 
@@ -40,10 +37,23 @@ rightClick = (e) => {
 }
 
 tagPieceButton.addEventListener("click", () => {
-    console.log(startIndex)
+    console.log(startIndex);
     console.log(endIndex);
     document.getElementById("tagContextMenu").style.display = "none";
+    document.getElementById("fragment_info").style.display = "block";
+    document.getElementById("fragment_text").value = selectedText;
+    document.getElementById("fragment_start_index").value = startIndex;
+    document.getElementById("fragment_end_index").value = endIndex;
+    highlightAbstract(startIndex, endIndex);
     startIndex = 0;
     endIndex = 0;
 })
 
+function highlightAbstract(startInd, endInd) {
+  let abstract = document.getElementById("abstract-text");
+  let abstractInnerHTML = abstract.innerHTML;
+  if (startInd >= 0) {
+   abstractInnerHTML = abstractInnerHTML.substring(0,startInd) + "<span class='abstract_highlight'>" + abstractInnerHTML.substring(startInd,endInd) + "</span>" + abstractInnerHTML.substring(endInd);
+   abstract.innerHTML = abstractInnerHTML;
+  }
+}
