70f05ce58e30a3f8fc0a43a6689f8f83e2d3724a
emrec <emre.calik01@gmail.com>
2021-12-13 23:18:52 +0300

    Prevent upvoting if already upvoted. #114

--- Project/Wikode/wikodeApp/utils/voteManager.py
+++ Project/Wikode/wikodeApp/utils/voteManager.py
@@ -9,11 +9,19 @@ class VoteManager:
         self.user_id = user_id
 
     def upVote(self, tagRelationId):
-        tag_relation = TagRelation.objects.get(pk=tagRelationId)
-        user = User.objects.get(pk=self.user_id)
+        if self.isNotUpVoted(tagRelationId):
+            tag_relation = TagRelation.objects.get(pk=tagRelationId)
+            user = User.objects.get(pk=self.user_id)
 
-        vote = Vote()
-        vote.tag_relation = tag_relation
-        vote.user = user
-        vote.vote = 1
-        vote.save()
+            vote = Vote()
+            vote.tag_relation = tag_relation
+            vote.user = user
+            vote.vote = 1
+            vote.save()
+
+    def isNotUpVoted(self, tagRelationId):
+        vote = Vote.objects.filter(user_id=self.user_id, tag_relation_id=tagRelationId)
+        if vote.exists() and vote.vote is 1:
+            return False
+        return True
+
