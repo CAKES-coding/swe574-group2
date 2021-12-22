9c1c3b335ad02613299f7ba27c5b04ee567bd929
emrec <emre.calik01@gmail.com>
2021-12-22 19:30:47 +0300

    Use datetime insead of timezone to comply with local time. #157

--- Project/Wikode/wikodeApp/models.py
+++ Project/Wikode/wikodeApp/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.postgres.search import SearchVectorField, SearchVector
 from django.contrib.postgres.fields import JSONField
-
+from datetime import datetime
 
 class RegistrationApplication(models.Model):
     applicationStatuses = (
@@ -135,7 +135,7 @@ class TagRelation(models.Model):
     start_index = models.IntegerField(null=True)
     end_index = models.IntegerField(null=True)
     vote_sum = models.IntegerField(default=0)
-    date = models.DateTimeField(default=timezone.now, blank=True)
+    date = models.DateTimeField(default=datetime.now(), blank=True)
     tagger = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
