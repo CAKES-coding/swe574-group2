ef1a1869a01d727624b8d95c2f86468376b86d13
jajimentol <aniljturan@gmail.com>
2021-12-27 16:58:25 +0300

    utility functions continued

--- Project/Wikode/wikodeApp/utils/suggestionManager.py
+++ Project/Wikode/wikodeApp/utils/suggestionManager.py
@@ -25,22 +25,7 @@ class SuggestionManager:
             self.followees = getFolloweeList(owner)
 
     def get_viewed_article_suggestion_from_followee(self):
-        article_list = []
-        article_id_list = self.get_article_id_list_from_followee()
-        viewed_article_id_list = self.get_my_viewed_id_list()
-
-        print(article_id_list)
-        print(viewed_article_id_list)
-
-        for viewed_id in viewed_article_id_list:
-            if viewed_id in article_id_list:
-                article_id_list[:] = (value for value in article_id_list if value != viewed_id)
-
-        for id in article_id_list:
-            article = Article.objects.get(id=id)
-            article_list.append(article)
-            print(article.id)
-
+        article_list = self.get_article_id_list_from_followee(activity_type=1)
         print(article_list)
         return article_list
 
@@ -53,26 +38,35 @@ class SuggestionManager:
 
     def get_tagged_articles_from_followee(self):
         article_list = []
-        if self.followees:
-            for followee in self.followees:
-                if isinstance(followee, User):
-                    followee_list = Activity.objects.filter(user_id=followee.id, target_type=2).exclude(user_id=self.user_id,
-                                                                                                 activity_type=1)
+        article_id_list = self.get_article_id_list_from_followee(activity_type=6)
+        viewed_article_id_list = self.get_my_viewed_id_list()
 
-    def get_article_id_list_from_followee(self):
+
+
+
+    def get_article_id_list_from_followee(self, activity_type):
         article_id_list = []
         if self.followees:
             for followee in self.followees:
                 if followee:
-                    followee_list = Activity.objects.filter(user_id=followee[0], activity_type=1)
+                    followee_list = Activity.objects.filter(user_id=followee[0], activity_type=activity_type, target_type=3)
                     for x in followee_list:
                         if x:
                             article_id_list.append(x.target_id)
-        return article_id_list
+
+        viewed_article_id_list = self.get_my_viewed_id_list()
+
+        for viewed_id in viewed_article_id_list:
+            if viewed_id in article_id_list:
+                article_id_list[:] = (value for value in article_id_list if value != viewed_id)
+
+
+        article_list = self.get_articles_from_id_list(id_list=article_id_list)
+        return article_list
 
     def get_my_viewed_id_list(self):
         viewed_article_id_list = []
-        my_viewed_list = Activity.objects.filter(user_id=self.user_id, activity_type=1)
+        my_viewed_list = Activity.objects.filter(user_id=self.user_id, activity_type=1, target_type=3)
         for viewed in my_viewed_list:
             if viewed:
                 if viewed.target_id in viewed_article_id_list:
@@ -80,3 +74,10 @@ class SuggestionManager:
                 else:
                     viewed_article_id_list.append(viewed.target_id)
         return viewed_article_id_list
+
+    def get_articles_from_id_list(self, id_list):
+        article_list = []
+        for id in id_list:
+            article = Article.objects.get(id=id)
+            article_list.append(article)
+        return article_list
