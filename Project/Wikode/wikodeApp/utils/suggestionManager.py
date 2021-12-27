c2fef8d01d817f8281747bd5ce498e92e1b15f73
jajimentol <aniljturan@gmail.com>
2021-12-27 19:01:15 +0300

    comments added to article suggestion methods

--- Project/Wikode/wikodeApp/utils/suggestionManager.py
+++ Project/Wikode/wikodeApp/utils/suggestionManager.py
@@ -18,6 +18,10 @@ class ArticleSuggestionRatings(Enum):
 
 class SuggestionManager:
 
+    # Limit for suggestion list
+    # Upon reach, the manger will stop finding more suggestions
+    suggestion_limit = 3
+
     def __init__(self, user_id):
         owner = User.objects.get(id=user_id)
         self.user_id = user_id
@@ -28,27 +32,30 @@ class SuggestionManager:
             self.followers = getFollowerList(owner)
             self.followees = getFolloweeList(owner)
 
+    # Ultimate method to get article suggestions
+    # returns until "suggestion_limit" is reached.
+    # gets random articles if the logic is not sufficient
     def get_article_suggestion(self):
         self.article_list.append(self.get_other_tagged_articles())
-        if len(self.article_list) >= 3:
+        if len(self.article_list) >= self.suggestion_limit:
             return self.article_list
 
         tagged_articles_from_followee = self.get_tagged_articles_from_followee()
         if tagged_articles_from_followee:
             self.article_list.append(self.get_tagged_articles_from_followee())
-            if len(self.article_list) >= 3:
+            if len(self.article_list) >= self.suggestion_limit:
                 return self.article_list
 
         viewed_article_suggestion_from_followee = self.get_viewed_article_suggestion_from_followee()
         if viewed_article_suggestion_from_followee:
             self.article_list.append(self.get_viewed_article_suggestion_from_followee())
-            if len(self.article_list) >= 3:
+            if len(self.article_list) >= self.suggestion_limit:
                 return self.article_list
 
         most_viewed_article = self.get_most_viewed_article()
         if most_viewed_article:
             self.article_list.append(self.get_most_viewed_article())
-            if len(self.article_list) >= 3:
+            if len(self.article_list) >= self.suggestion_limit:
                 return self.article_list
 
         recently_tagged_article = self.get_recently_tagged_article()
@@ -59,10 +66,15 @@ class SuggestionManager:
         return self.article_list
 
 
+    # Gets viewed articles from followee
+    # excludes owner's already viewed articles
     def get_viewed_article_suggestion_from_followee(self):
         article_list = self.get_article_id_list_from_followee(activity_type=1)
         return article_list
 
+
+    # Gets the most viewed article (1)
+    # chekcs if the owner has already seen it
     def get_most_viewed_article(self):
         most_common = Activity.objects.filter(activity_type=1,target_type=3).annotate(mc=Count('target_id')).order_by('-mc')[0]
         if most_common:
@@ -79,11 +91,15 @@ class SuggestionManager:
                     return article
 
 
+    # Gets tagged articles from followee
+    # excludes owner's already viewed articles
     def get_tagged_articles_from_followee(self):
         article_list = self.get_article_id_list_from_followee(activity_type=6)
         return article_list
 
 
+    # Gets recently tagged articles from everyone
+    # excludes owner's already viewed articles
     def get_recently_tagged_article(self):
         latest_tagged_article_list = []
         tagged_article = Activity.objects.filter(activity_type=6,target_type=3).order_by('-id')
@@ -100,6 +116,9 @@ class SuggestionManager:
                 self.article_id_list.append(id)
         return article_list
 
+
+    # Gets other articles that is tagged with same tags
+    # excludes owner's already viewed articles
     def get_other_tagged_articles(self):
         my_tags = TagRelation.objects.filter(tagger=self.owner)
         my_tags_id_list = []
@@ -127,7 +146,8 @@ class SuggestionManager:
         return article_list
 
 
-
+    # Returns an article list from followee
+    # depending on activity type
     def get_article_id_list_from_followee(self, activity_type):
         article_id_list = []
         if self.followees:
@@ -143,6 +163,8 @@ class SuggestionManager:
         article_list = self.get_articles_from_id_list(id_list=article_id_list)
         return article_list
 
+
+    # Returns owner's already viewed article list
     def get_my_viewed_id_list(self):
         viewed_article_id_list = []
         my_viewed_list = Activity.objects.filter(user_id=self.user_id, activity_type=1, target_type=3)
@@ -154,6 +176,8 @@ class SuggestionManager:
                     viewed_article_id_list.append(viewed.target_id)
         return viewed_article_id_list
 
+
+    # Returns an article list, from a given id list
     def get_articles_from_id_list(self, id_list):
         article_list = []
         for id in id_list:
@@ -165,6 +189,8 @@ class SuggestionManager:
                 self.article_id_list.append(id)
         return article_list
 
+
+    # substract an id list from owner's already viewed articles id list
     def substract_viewed_articles(self, id_list):
         viewed_article_id_list = self.get_my_viewed_id_list()
         for viewed_id in viewed_article_id_list:
@@ -172,10 +198,17 @@ class SuggestionManager:
                 id_list[:] = (value for value in id_list if value != viewed_id)
         return id_list
 
+
+    # Finds a random article, until the limit is reached
+    # excludes viewed articles
     def get_random_article(self):
-        while(len(self.article_list) < 3):
+        viewed_article_id_list = self.get_my_viewed_id_list()
+        while(len(self.article_list) < self.suggestion_limit):
             article = random.choice(Article.objects.all())
-            if article.id in self.article_list:
+            if article.id in self.article_id_list:
                 continue
             else:
-                self.article_list.append(article)
+                if article.id in viewed_article_id_list:
+                    continue
+                else:
+                    self.article_list.append(article)
